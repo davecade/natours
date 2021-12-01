ad928c76d7770ad7332f01704b96308e734fac4f
Dave <dave.cadelina@outlook.com>
2021-12-02 00:41:06 +1100

    added params to API

--- app.js
+++ app.js
@@ -1,48 +1,67 @@
 const express = require('express');
-const fs = require('fs')
+const fs = require('fs');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 //-- sends back all the tours
 app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  if (Number(req.params.id) > tours.length-1) {
+    return res.status(404).json({
+      status: '404 Not FOund',
+    });
+  }
+
+  const tour = tours.find((item) => item.id === Number(req.params.id));
+  if (tour) {
     res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours: tours
-        }
-    })
-})
+      status: 'success',
+      data: {
+        ...tour,
+      },
+    });
+  }
+});
 
 app.post('/api/v1/tours', (req, res) => {
-    const newId = tours[tours.length-1].id + 1
-    console.log(req.body)
-    const newTour = {
-        id: newId,
-        ...req.body
+  const newId = tours[tours.length - 1].id + 1;
+  console.log(req.body);
+  const newTour = {
+    id: newId,
+    ...req.body,
+  };
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
     }
-
-    tours.push(newTour)
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        err => {
-            res.status(201).json({
-                status: 'success',
-                data: {
-                    tour: newTour
-                }
-            })
-        }
-    )
-
-})
+  );
+});
 
 const port = 4000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
-})
+  console.log(`App running on port ${port}...`);
+});
