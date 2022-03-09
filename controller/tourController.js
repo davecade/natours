a2333795fc6fc11d93aa8b07896fda116137eed1
Dave <dave.cadelina@outlook.com>
2022-03-09 21:06:35 +1100

    added delete tour

--- controller/tourController.js
+++ controller/tourController.js
@@ -80,11 +80,21 @@ const updateTour = async (req, res) => {
   }
 };
 
-const deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({
+      status: 'fail',
+      message: e,
+    });
+  }
 };
 
 module.exports = {
